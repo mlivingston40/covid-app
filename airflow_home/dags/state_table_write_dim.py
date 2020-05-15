29ded6adc1b7ff605dbc139423548cf9755af90c
mjlivingston40 <matt.j.livingston.40@gmail.com>
2020-05-15 16:09:49 -0400

    first dag state_table_write_dim, runs on local server

--- airflow_home/dags/state_table_write_dim.py
+++ airflow_home/dags/state_table_write_dim.py
@@ -1,13 +1,33 @@
-
-# TODO: Task 1 -- http response from end point - HttpSensor
-
-# TODO: Task 2 -- request all avail states (feed output to Task 3) - PythonOperator (scripts/states_dim.py)
-# TODO:              -- write/replace covidtracking.states
-
+import airflow
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
 
 
+default_args = {
+            "owner": "Airflow",
+            "start_date": airflow.utils.dates.days_ago(1),
+            "depends_on_past": False,
+            "email_on_failure": False,
+            "email_on_retry": False,
+            "email": "matt@aaaio.solutions",
+            "retries": 1,
+            "retry_delay": timedelta(minutes=5)
+        }
 
+with DAG(dag_id="state_table_write_dim", schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
 
-with DAG(dag_id="state_table_write_dim", )
+    states_dim_defensive = BashOperator(
+            task_id="states_dim_defensive",
+            bash_command="python /Users/mlivingston/Documents/covid-app/airflow_home/dags/scripts/"
+                         "states_dim_defensive.py"
+    )
 
-# task_1 >> task_2
+    states_dim = BashOperator(
+            task_id="states_dim",
+            bash_command="python /Users/mlivingston/Documents/covid-app/airflow_home/dags/scripts/"
+                         "states_dim.py"
+    )
+
+    states_dim_defensive >> states_dim
+
